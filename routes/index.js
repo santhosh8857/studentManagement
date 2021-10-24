593d7b00c1a387dd10fe26df48707a77789f5a68
unknown <santhosh8857@gmail.com>
2021-10-24 21:28:34 +0530

    added post delete

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var { ObjectId } = require("mongodb");
 const { mongodb, MongoClient, dbUrl } = require("../library/dbConfig");
 
 /* GET home page. */
@@ -12,7 +13,7 @@ router.get("/all-students", async (req, res) => {
 
     res.send({
       message: "Success",
-      details: data,
+      data: data,
     });
   } catch (e) {
     console.log(e);
@@ -34,6 +35,8 @@ router.post("/add-many-students", async (req, res) => {
   } catch (e) {
     console.log(e);
     res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
   }
 });
 
@@ -46,6 +49,58 @@ router.post("/add-student", async (req, res) => {
     const data = await db.collection("students").insertOne(req.body);
 
     res.send({ message: "Inserted!", details: data });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
+});
+
+// edit student
+router.put("/edit-student/:id", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("studentManagement");
+    const data = await db.collection("students").updateOne(
+      { _id: ObjectId(req.params.id) },
+      {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          mobile: req.body.mobile,
+          class: req.body.class,
+        },
+      }
+    );
+
+    res.send({
+      message: "Updated",
+      details: data,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
+});
+
+// delete user
+router.delete("/delete-user/:id", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("studentManagement");
+    const data = await db
+      .collection("students")
+      .deleteOne({ _id: ObjectId(req.params.id) });
+
+    res.send({
+      message: "Deleted!",
+      data: data,
+    });
   } catch (e) {
     console.log(e);
     res.send({ message: "Error in connection" });
