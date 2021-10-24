71f4c5165112ae9b8fc4852999d0154f54bdac31
unknown <santhosh8857@gmail.com>
2021-10-24 19:36:46 +0530

    added get post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { mongodb, MongoClient, dbUrl } = require("../library/dbConfig");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/all-students", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("studentManagement");
+    const data = await db.collection("students").find().toArray();
+
+    res.send({
+      message: "Success",
+      details: data,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
 });
 
+// add students
+router.post("/add-many-students", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("studentManagement");
+    const data = await db.collection("students").insertMany(req.body);
+
+    res.send({ message: "Inserted!", details: data });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Error in connection" });
+  }
+});
+
+// add student
+router.post("/add-student", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("studentManagement");
+    const data = await db.collection("students").insertOne(req.body);
+
+    res.send({ message: "Inserted!", details: data });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
+});
 module.exports = router;
